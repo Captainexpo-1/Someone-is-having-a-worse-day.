ddc18aaa53e219ef6e3744655cf2b01a3329b464
Elliot Throop <captainexpo@gmail.com>
2023-01-01 17:42:39 -0500

    optimizations and fixes

--- index.js
+++ index.js
@@ -1,57 +1,47 @@
-var depth = -1
-var loaded = []
-var titles = []
+var depth = 0;
+var loaded = [];
+var titles = [];
+var test = 0
+    
 
-
-function load(){
-  (function () {
-    const rows = document.querySelectorAll('.row');
-    rows.forEach(row => row.remove());
-
-    document.getElementById("bt").innerHTML = 'Feel Better'
+    //document.getElementById("bt").innerHTML = 'Feel Better';
     var container = document.querySelector(".container");
-    if(depth===-1){
-        
-    }
+
     reddit.top("wallstreetbets").t("week").limit(5000).fetch(function (res) {
       for (var i = 0; i < res.data.children.length; i++) {
         
         var betData = res.data.children[i].data; //get the data for this post
         var bet = document.createElement("img");
-        var row = document.createElement("div");
-        var right = document.createElement("div");
-
-
-        row.className = "row";
-        right.className = "col-xs-8";
-        bet.id = 'row'
-
+        bet.id = 'row';
         bet.setAttribute("src", betData.url); //set url of image to reddit post
-
+        bet.className = 'bigimg';
+        /* ----- not needed ----- */
+        
         if(betData.post_hint === 'image' && betData.link_flair_text === 'Loss'){
           loaded.push(bet) //add this image to the list 
           console.log('compatable on #' + i)
-          right.appendChild(loaded[depth]);
-          titles.push(betData.title)
-          row.appendChild(right);
-          container.appendChild(row);
+          titles.push(betData.title); //push title
+          
         }else{
-            console.log("not compatable on: " + i)
-        }
-        
-        
-
-        bet.className = 'bigimg'
-        if(i === depth){
-          document.getElementById('caption').innerText = titles[depth];
+            console.log("not compatable on: " + i);
         }
         
         
       }
-    });
-    depth += 1
+   
+    test+=5
+    console.log(test)
     
     
-    
-  })();
+  });
+  console.log(loaded)
+
+function load(){
+    const rows = document.querySelectorAll('.bigimg');
+    rows.forEach(row => row.remove());
+    depth++
+    console.log(typeof container)
+    document.getElementById('container').appendChild(loaded[depth]);
+    document.getElementById('caption').innerText = titles[depth];
+   
 }
