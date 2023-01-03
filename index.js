d26b3f43db8f91fc80fde7167ab3da3d0221609d
Elliot Throop <captainexpo@gmail.com>
2023-01-03 07:24:06 -0500

    added the ability to go to a therapist.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var test = 0
     //document.getElementById("bt").innerHTML = 'Feel Better';
     var container = document.querySelector(".container");
 
-    reddit.top("wallstreetbets").t("week").limit(5000).fetch(function (res) {
+    reddit.top("wallstreetbets").t('week').limit(50000).fetch(function (res) {
       for (var i = 0; i < res.data.children.length; i++) {
         
         var betData = res.data.children[i].data; //get the data for this post
@@ -16,10 +16,15 @@ var test = 0
         bet.setAttribute("src", betData.url); //set url of image to reddit post
         bet.className = 'bigimg';
         
-        if(betData.post_hint === 'image' && betData.link_flair_text === 'Loss'){
-          loaded.push(bet) //add this image to the list 
-          console.log('compatable on #' + i)
-          titles.push(betData.title); //push title
+        if(betData.post_hint === 'image'){
+            if(betData.link_flair_text === 'Loss' || betData.link_flair_text === 'Meme'){
+                loaded.push(bet) //add this image to the list 
+                console.log('compatable on #' + i)
+                titles.push(betData.title); //push title
+            }
+            else{
+                console.log('not compatable on #' + i)
+            }
           
         }else{
             console.log("not compatable on: " + i);
@@ -36,11 +41,14 @@ var test = 0
   console.log(loaded)
 
 function load(){
-    const rows = document.querySelectorAll('.bigimg');
-    rows.forEach(row => row.remove());
+  const rows = document.querySelectorAll('.bigimg');
+  rows.forEach(row => row.remove());
+  if(depth+1 < loaded.length){
     depth++
     console.log(typeof container)
     document.getElementById('container').appendChild(loaded[depth]);
     document.getElementById('caption').innerText = titles[depth];
-   
+  }else{
+    window.location.href = 'https://www.google.com/search?q=therapists+near+me&rlz=1C5CHFA_enUS1037US1038&oq=therapist&aqs=chrome.0.0i131i433i457i512j69i57j0i402l2j0i433i512j0i131i433i512j0i433i512j0i512j0i433i512j0i512.2375j0j7&sourceid=chrome&ie=UTF-8'
+  }
 }
