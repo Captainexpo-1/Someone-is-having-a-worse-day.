e23530346e0ef95aeba1ff6f91b8b5cfd92703f3
Elliot Throop <captainexpo@gmail.com>
2023-01-04 17:01:22 -0500

    you need to wait for it to load.

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 var depth = 0;
 var loaded = [];
 var titles = [];
+
+var isloaded = false
 var test = 0
     
 
-    //document.getElementById("bt").innerHTML = 'Feel Better';
-    var container = document.querySelector(".container");
+//document.getElementById("bt").innerHTML = 'Feel Better';
+var container = document.querySelector(".container");
 
-    reddit.top("wallstreetbets").t('week').limit(50000).fetch(function (res) {
-      for (var i = 0; i < res.data.children.length; i++) {
+reddit.top("wallstreetbets").t('week').limit(50000).fetch(function (res) {
+  for (var i = 0; i < res.data.children.length; i++) {
         
-        var betData = res.data.children[i].data; //get the data for this post
-        var bet = document.createElement("img");
-        bet.id = 'row';
-        bet.setAttribute("src", betData.url); //set url of image to reddit post
-        bet.className = 'bigimg';
+    var betData = res.data.children[i].data; //get the data for this post
+    var bet = document.createElement("img");
+    bet.id = 'row';
+    bet.setAttribute("src", betData.url); //set url of image to reddit post
+    bet.className = 'bigimg';
         
-        if(betData.post_hint === 'image'){
-            if(betData.link_flair_text === 'Loss' || betData.link_flair_text === 'Meme'){
-                loaded.push(bet) //add this image to the list 
-                console.log('compatable on #' + i)
-                titles.push(betData.title); //push title
-            }
-            else{
-                console.log('not compatable on #' + i)
-            }
-          
-        }else{
-            console.log("not compatable on: " + i);
+    if(betData.post_hint === 'image'){
+        if(betData.link_flair_text === 'Loss' || betData.link_flair_text === 'Meme'){
+            loaded.push(bet) //add this image to the list 
+            console.log('compatable on #' + i)
+            titles.push(betData.title); //push title
         }
+        else{
+            console.log('not compatable on #' + i)
+        }
+          
+    }else{
+        console.log("not compatable on: " + i);
+    }
         
         
-      }
-   
-    test+=5
-    console.log(test)
+  }
+  isloaded = true
+  document.getElementById('caption').innerText = 'go ahead :)'
+test+=5
+console.log(test)
     
     
-  });
-  console.log(loaded)
+});
+
 
 function load(){
+  if(!isloaded){return 0}
   const rows = document.querySelectorAll('.bigimg');
   rows.forEach(row => row.remove());
   if(depth+1 < loaded.length){
